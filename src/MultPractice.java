32118b0abc5f38982234603397233ba8c2dba008
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-04-01 08:27:20 -0400

    yes

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,17 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice {
+    private int a;
+    private int b;
+    public MultPractice(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    public String getProblem(){
+        return a+" Times "+b;
+    }
+    public void nextProblem(){
+        b++;
+    }
 }
